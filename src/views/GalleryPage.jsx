bc72699e1e7079fc0ae5b0a19025e6093d50d304
gokul-2121 <gokulmani2006kl012121@gmail.com>
2026-05-01 23:34:29 +0530

    remove: View More Images on Google Drive button from gallery page

--- src/views/GalleryPage.jsx
+++ src/views/GalleryPage.jsx
@@ -12,20 +12,6 @@ export default function GalleryPage({ galleryItems = [] }) {
         </div>
       </div>
       <Gallery images={galleryItems} />
-      <div className="container" style={{ textAlign: 'center', paddingBottom: '4rem' }}>
-        <a 
-          href="https://drive.google.com/drive/folders/1wB1oPEEt30vM22qnWcD2SdrCrVmhOYk1" 
-          target="_blank" 
-          rel="noopener noreferrer"
-          className="btn btn-primary"
-          style={{ display: 'inline-flex', alignItems: 'center', gap: '0.5rem' }}
-        >
-          <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M7 11L12 6L17 11M12 18V7" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-          </svg>
-          View More Images on Google Drive
-        </a>
-      </div>
     </main>
   )
 }
